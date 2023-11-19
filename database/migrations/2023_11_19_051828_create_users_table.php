931048534b9bc8d423c51d6c350f43416f0265e3
devinluize <71080055+devinluize@users.noreply.github.com>
2023-11-19 13:35:00 +0700

    add user migration

--- database/migrations/2023_11_19_051828_create_users_table.php
+++ database/migrations/2023_11_19_051828_create_users_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('tb_user', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->string('username')->unique();
+            $table->string('email');
+            $table ->string('password')->default(null);
             $table->timestamps();
         });
     }
