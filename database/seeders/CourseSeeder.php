a677df4cfd711089599edd15257ee6e013cdfe48
devinluize <71080055+devinluize@users.noreply.github.com>
2023-12-10 22:26:57 +0700

    Update CourseSeeder.php

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Course;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,5 +14,17 @@ class CourseSeeder extends Seeder
     public function run(): void
     {
         //
+        Course::create([
+            'course_name'=>'Roma Tomato',
+            'course_detail'=>'Roma Tomato'
+        ]);
+        Course::create([
+            'course_name'=>'Lady Slipper Orchid',
+            'course_detail'=>'Lady Slipper Orchid'
+        ]);
+        Course::create([
+            'course_name'=>'Spinach',
+            'course_detail'=>'Spinach'
+        ]);
     }
 }
