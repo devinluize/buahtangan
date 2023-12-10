630e313c906fb78b48c0e0510b9ad90d82be7e2e
devinluize <71080055+devinluize@users.noreply.github.com>
2023-12-10 22:12:49 +0700

    Update Sub.php

--- app/Models/Sub.php
+++ app/Models/Sub.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Sub extends Model
 {
+    protected $table='subs';
+    protected $primaryKey='subs_id';
+    protected $fillable = [
+        'user_id',
+        'course_id',
+        'step'
+    ];
     use HasFactory;
+    public function User(): BelongsTo{
+        return $this->BelongsTo(User::class, 'user_id');
+    }
+    public function Course(): BelongsTo{
+        return $this->BelongsTo(Course::class, 'course_id');
+    }
 }
