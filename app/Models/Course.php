db613f0d082b881bc1c16214d842eba9939e45c0
devinluize <71080055+devinluize@users.noreply.github.com>
2023-12-10 22:12:45 +0700

    Update Course.php

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Course extends Model
 {
     use HasFactory;
+    protected $table='courses';
+    protected $primaryKey='course_id';
+    protected $fillable = [
+        'course_name',
+        'course_detail',
+    ];
+    public function Sub(): HasMany{
+        return $this->hasMany(Sub::class, 'subs_id');
+    }
 }
