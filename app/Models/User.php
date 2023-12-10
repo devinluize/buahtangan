f980e86008efd871d6f9ac373cf7d4cabd8907ba
devinluize <71080055+devinluize@users.noreply.github.com>
2023-12-10 22:12:52 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
@@ -14,4 +15,7 @@ class User extends Authenticatable
     protected $fillable=['email',
     'username'
     ,'password'];
+    public function Sub(): HasMany{
+        return $this->hasMany(Sub::class, 'subs_id');
+    }
 }
