0a950bc55b87d93f668b4c9efa556b5b0933e847
devinluize <71080055+devinluize@users.noreply.github.com>
2023-12-10 22:27:00 +0700

    Update StepSeeder.php

--- database/seeders/StepSeeder.php
+++ database/seeders/StepSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Step;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,85 @@ class StepSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Step::create([
+            'title'=>'Preparing Soil',
+            'step_desc'=>'Lets Prepare Soil for the little Fella!',
+            'step_desc_first'=>"Preparing soil for spinach is crucial to ensure a successful and bountiful harvest. Spinach thrives in well-draining, nutrient-rich soil. Start by selecting a sunny to partially shaded location in your garden. Spinach prefers cooler temperatures, so choose a spot that gets morning sun and afternoon shade, especially in warmer climates.",
+            'step_desc_second'=>"To prepare the soil, begin by removing any weeds, rocks, or debris from the area. Loosen the soil with a garden fork or tiller to a depth of about 6-8 inches. This will improve soil aeration and drainage. Add organic matter like compost or well-rotted manure to the soil. Mix it in thoroughly to enhance soil fertility and improve moisture retention. Aim to achieve a slightly acidic to neutral pH level, typically around 6.5. This will create optimal growing conditions for your spinach. Finally, level the soil surface and create rows or beds for planting, leaving adequate space between rows to ensure good air circulation and easy harvesting.
+
+            Spinach prefers cool, moist conditions, so be sure to maintain consistent soil moisture throughout the growing season. Applying a layer of mulch can help retain moisture and reduce weed competition. With well-prepared soil and proper care, you can enjoy a healthy and productive spinach crop in your garden."
+        ]);
+        Step::create([
+            'title'=>'Planting',
+            'step_desc'=>'How would you like to plant them?',
+            'step_desc_first'=>"Preparing soil for spinach is crucial to ensure a successful and bountiful harvest. Spinach thrives in well-draining, nutrient-rich soil. Start by selecting a sunny to partially shaded location in your garden. Spinach prefers cooler temperatures, so choose a spot that gets morning sun and afternoon shade, especially in warmer climates.",
+            'step_desc_second'=>"To prepare the soil, begin by removing any weeds, rocks, or debris from the area. Loosen the soil with a garden fork or tiller to a depth of about 6-8 inches. This will improve soil aeration and drainage. Add organic matter like compost or well-rotted manure to the soil. Mix it in thoroughly to enhance soil fertility and improve moisture retention. Aim to achieve a slightly acidic to neutral pH level, typically around 6.5. This will create optimal growing conditions for your spinach. Finally, level the soil surface and create rows or beds for planting, leaving adequate space between rows to ensure good air circulation and easy harvesting.
+
+            Spinach prefers cool, moist conditions, so be sure to maintain consistent soil moisture throughout the growing season. Applying a layer of mulch can help retain moisture and reduce weed competition. With well-prepared soil and proper care, you can enjoy a healthy and productive spinach crop in your garden."
+        ]);
+        Step::create([
+            'title'=>'Picture the lil fella!',
+            'step_desc'=>'Capture this special part of their life!',
+            'step_desc_first'=>"Preparing soil for spinach is crucial to ensure a successful and bountiful harvest. Spinach thrives in well-draining, nutrient-rich soil. Start by selecting a sunny to partially shaded location in your garden. Spinach prefers cooler temperatures, so choose a spot that gets morning sun and afternoon shade, especially in warmer climates.",
+            'step_desc_second'=>"To prepare the soil, begin by removing any weeds, rocks, or debris from the area. Loosen the soil with a garden fork or tiller to a depth of about 6-8 inches. This will improve soil aeration and drainage. Add organic matter like compost or well-rotted manure to the soil. Mix it in thoroughly to enhance soil fertility and improve moisture retention. Aim to achieve a slightly acidic to neutral pH level, typically around 6.5. This will create optimal growing conditions for your spinach. Finally, level the soil surface and create rows or beds for planting, leaving adequate space between rows to ensure good air circulation and easy harvesting.
+
+            Spinach prefers cool, moist conditions, so be sure to maintain consistent soil moisture throughout the growing season. Applying a layer of mulch can help retain moisture and reduce weed competition. With well-prepared soil and proper care, you can enjoy a healthy and productive spinach crop in your garden."
+        ]);
+        Step::create([
+            'title'=>'Watering',
+            'step_desc'=>'How much do they drink?',
+            'step_desc_first'=>"Preparing soil for spinach is crucial to ensure a successful and bountiful harvest. Spinach thrives in well-draining, nutrient-rich soil. Start by selecting a sunny to partially shaded location in your garden. Spinach prefers cooler temperatures, so choose a spot that gets morning sun and afternoon shade, especially in warmer climates.",
+            'step_desc_second'=>"To prepare the soil, begin by removing any weeds, rocks, or debris from the area. Loosen the soil with a garden fork or tiller to a depth of about 6-8 inches. This will improve soil aeration and drainage. Add organic matter like compost or well-rotted manure to the soil. Mix it in thoroughly to enhance soil fertility and improve moisture retention. Aim to achieve a slightly acidic to neutral pH level, typically around 6.5. This will create optimal growing conditions for your spinach. Finally, level the soil surface and create rows or beds for planting, leaving adequate space between rows to ensure good air circulation and easy harvesting.
+
+            Spinach prefers cool, moist conditions, so be sure to maintain consistent soil moisture throughout the growing season. Applying a layer of mulch can help retain moisture and reduce weed competition. With well-prepared soil and proper care, you can enjoy a healthy and productive spinach crop in your garden."
+        ]);
+        Step::create([
+            'title'=>'Fertilizing',
+            'step_desc'=>'How much do they eat?',
+            'step_desc_first'=>"Preparing soil for spinach is crucial to ensure a successful and bountiful harvest. Spinach thrives in well-draining, nutrient-rich soil. Start by selecting a sunny to partially shaded location in your garden. Spinach prefers cooler temperatures, so choose a spot that gets morning sun and afternoon shade, especially in warmer climates.",
+            'step_desc_second'=>"To prepare the soil, begin by removing any weeds, rocks, or debris from the area. Loosen the soil with a garden fork or tiller to a depth of about 6-8 inches. This will improve soil aeration and drainage. Add organic matter like compost or well-rotted manure to the soil. Mix it in thoroughly to enhance soil fertility and improve moisture retention. Aim to achieve a slightly acidic to neutral pH level, typically around 6.5. This will create optimal growing conditions for your spinach. Finally, level the soil surface and create rows or beds for planting, leaving adequate space between rows to ensure good air circulation and easy harvesting.
+
+            Spinach prefers cool, moist conditions, so be sure to maintain consistent soil moisture throughout the growing season. Applying a layer of mulch can help retain moisture and reduce weed competition. With well-prepared soil and proper care, you can enjoy a healthy and productive spinach crop in your garden."
+        ]);
+        Step::create([
+            'title'=>'Picture the growing fella!',
+            'step_desc'=>'To see and remember their growth',
+            'step_desc_first'=>"Preparing soil for spinach is crucial to ensure a successful and bountiful harvest. Spinach thrives in well-draining, nutrient-rich soil. Start by selecting a sunny to partially shaded location in your garden. Spinach prefers cooler temperatures, so choose a spot that gets morning sun and afternoon shade, especially in warmer climates.",
+            'step_desc_second'=>"To prepare the soil, begin by removing any weeds, rocks, or debris from the area. Loosen the soil with a garden fork or tiller to a depth of about 6-8 inches. This will improve soil aeration and drainage. Add organic matter like compost or well-rotted manure to the soil. Mix it in thoroughly to enhance soil fertility and improve moisture retention. Aim to achieve a slightly acidic to neutral pH level, typically around 6.5. This will create optimal growing conditions for your spinach. Finally, level the soil surface and create rows or beds for planting, leaving adequate space between rows to ensure good air circulation and easy harvesting.
+
+            Spinach prefers cool, moist conditions, so be sure to maintain consistent soil moisture throughout the growing season. Applying a layer of mulch can help retain moisture and reduce weed competition. With well-prepared soil and proper care, you can enjoy a healthy and productive spinach crop in your garden."
+        ]);
+        Step::create([
+            'title'=>'Take care of Spinach',
+            'step_desc'=>'Tak care of their special needs!',
+            'step_desc_first'=>"Preparing soil for spinach is crucial to ensure a successful and bountiful harvest. Spinach thrives in well-draining, nutrient-rich soil. Start by selecting a sunny to partially shaded location in your garden. Spinach prefers cooler temperatures, so choose a spot that gets morning sun and afternoon shade, especially in warmer climates.",
+            'step_desc_second'=>"To prepare the soil, begin by removing any weeds, rocks, or debris from the area. Loosen the soil with a garden fork or tiller to a depth of about 6-8 inches. This will improve soil aeration and drainage. Add organic matter like compost or well-rotted manure to the soil. Mix it in thoroughly to enhance soil fertility and improve moisture retention. Aim to achieve a slightly acidic to neutral pH level, typically around 6.5. This will create optimal growing conditions for your spinach. Finally, level the soil surface and create rows or beds for planting, leaving adequate space between rows to ensure good air circulation and easy harvesting.
+
+            Spinach prefers cool, moist conditions, so be sure to maintain consistent soil moisture throughout the growing season. Applying a layer of mulch can help retain moisture and reduce weed competition. With well-prepared soil and proper care, you can enjoy a healthy and productive spinach crop in your garden."
+        ]);
+        Step::create([
+            'title'=>'Planting',
+            'step_desc'=>'How would you like to plant them?',
+            'step_desc_first'=>"Preparing soil for spinach is crucial to ensure a successful and bountiful harvest. Spinach thrives in well-draining, nutrient-rich soil. Start by selecting a sunny to partially shaded location in your garden. Spinach prefers cooler temperatures, so choose a spot that gets morning sun and afternoon shade, especially in warmer climates.",
+            'step_desc_second'=>"To prepare the soil, begin by removing any weeds, rocks, or debris from the area. Loosen the soil with a garden fork or tiller to a depth of about 6-8 inches. This will improve soil aeration and drainage. Add organic matter like compost or well-rotted manure to the soil. Mix it in thoroughly to enhance soil fertility and improve moisture retention. Aim to achieve a slightly acidic to neutral pH level, typically around 6.5. This will create optimal growing conditions for your spinach. Finally, level the soil surface and create rows or beds for planting, leaving adequate space between rows to ensure good air circulation and easy harvesting.
+
+            Spinach prefers cool, moist conditions, so be sure to maintain consistent soil moisture throughout the growing season. Applying a layer of mulch can help retain moisture and reduce weed competition. With well-prepared soil and proper care, you can enjoy a healthy and productive spinach crop in your garden."
+        ]);
+        Step::create([
+            'title'=>'Harvesting',
+            'step_desc'=>'When to harvest?',
+            'step_desc_first'=>"Preparing soil for spinach is crucial to ensure a successful and bountiful harvest. Spinach thrives in well-draining, nutrient-rich soil. Start by selecting a sunny to partially shaded location in your garden. Spinach prefers cooler temperatures, so choose a spot that gets morning sun and afternoon shade, especially in warmer climates.",
+            'step_desc_second'=>"To prepare the soil, begin by removing any weeds, rocks, or debris from the area. Loosen the soil with a garden fork or tiller to a depth of about 6-8 inches. This will improve soil aeration and drainage. Add organic matter like compost or well-rotted manure to the soil. Mix it in thoroughly to enhance soil fertility and improve moisture retention. Aim to achieve a slightly acidic to neutral pH level, typically around 6.5. This will create optimal growing conditions for your spinach. Finally, level the soil surface and create rows or beds for planting, leaving adequate space between rows to ensure good air circulation and easy harvesting.
+
+            Spinach prefers cool, moist conditions, so be sure to maintain consistent soil moisture throughout the growing season. Applying a layer of mulch can help retain moisture and reduce weed competition. With well-prepared soil and proper care, you can enjoy a healthy and productive spinach crop in your garden."
+        ]);
+        Step::create([
+            'title'=>'Picture the harvest results!',
+            'step_desc'=>'Yum Yum!',
+            'step_desc_first'=>"Preparing soil for spinach is crucial to ensure a successful and bountiful harvest. Spinach thrives in well-draining, nutrient-rich soil. Start by selecting a sunny to partially shaded location in your garden. Spinach prefers cooler temperatures, so choose a spot that gets morning sun and afternoon shade, especially in warmer climates.",
+            'step_desc_second'=>"To prepare the soil, begin by removing any weeds, rocks, or debris from the area. Loosen the soil with a garden fork or tiller to a depth of about 6-8 inches. This will improve soil aeration and drainage. Add organic matter like compost or well-rotted manure to the soil. Mix it in thoroughly to enhance soil fertility and improve moisture retention. Aim to achieve a slightly acidic to neutral pH level, typically around 6.5. This will create optimal growing conditions for your spinach. Finally, level the soil surface and create rows or beds for planting, leaving adequate space between rows to ensure good air circulation and easy harvesting.
+
+            Spinach prefers cool, moist conditions, so be sure to maintain consistent soil moisture throughout the growing season. Applying a layer of mulch can help retain moisture and reduce weed competition. With well-prepared soil and proper care, you can enjoy a healthy and productive spinach crop in your garden."
+        ]);
     }
 }
