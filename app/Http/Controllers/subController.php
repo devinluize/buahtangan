55bb10cb31cdbfaba662af06c49f826fa2272804
devinluize <71080055+devinluize@users.noreply.github.com>
2023-12-13 23:09:49 +0700

    Update subController.php

--- app/Http/Controllers/subController.php
+++ app/Http/Controllers/subController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Sub;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class subController extends Controller
 {
     //
     public function add_sub($id){
         Sub::create([
-            'user_id'=>1,
+            'user_id'=>Auth::id(),
             'course_id'=>$id,
             'step'=>1
         ]);
